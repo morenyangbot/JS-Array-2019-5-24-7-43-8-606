bd344fc81760b9d7a4399eb736a15d079e6b1f29
morenyangbot <45971234+morenyangbot@users.noreply.github.com>
2019-07-01 20:41:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,54 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
-
+console.log(typeof a === 'object');
+console.log(typeof b === 'object');
 
 // 编写程序，将下面数组中的每一项都乘以2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
-
+for (let i = 0; i < a.length; i++) {
+  a[i] = a[i] * 2;
+}
 
 // 编写程序，按下面的要求输出结果。
-var colors = ["Red", "Green", "White", "Black"];
+var colors = ['Red', 'Green', 'White', 'Black'];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
-
+console.log(colors.join(' '));
+console.log(colors.join('+'));
+console.log(colors.join(','));
 
 // 编写程序，将下面数组中的数字按从大到小的顺序排序。
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
-
+function f(a, b) {
+  return b - a;
+}
+a.sort(f);
 
 // 编程程序，找出下列数组中出现频率最高的元素。
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+function findMost(arr) {
+  let map = new Map();
+  a.forEach(item => {
+    if (map.has(item)) {
+      map.set(item, map.get(item) + 1);
+    } else {
+      map.set(item, 1);
+    }
+  });
+  let max;
+  let maxIndex;
+  map.forEach((val, key) => {
+    if (!max || max < val) {
+      max = val;
+      maxIndex = key;
+    }
+  });
+  console.log(maxIndex);
+  return maxIndex;
+}
+findMost(a);
